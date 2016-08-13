24cde24b0cfc5efdbcd502110f1615b5be38a36b
Leonardo Cadastro <Leonardo Cadastro>
2016-08-13 00:02:56 -0300

    add create books

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -3,7 +3,11 @@ function ProdutosDAO(connection) {
 }
 
 ProdutosDAO.prototype.lista = function(callback) {
-	this._connection.query('select * from Livros', callback)
+	this._connection.query('select * from livros', callback)
+}
+
+ProdutosDAO.prototype.salva = function(produto, callback) {
+	this._connection.query('insert into livros set ?', produto, callback)
 }
 
 module.exports = function() {
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,6 @@
 module.exports = function(app) {
-	app.get('/produtos', function(req, res) {
+
+	var listaProdutos = function(req, res) {
 		var connection = app.infra.connectionFactory();
 		var produtosDAO = new app.infra.ProdutosDAO(connection);
 
@@ -10,5 +11,24 @@ module.exports = function(app) {
 		});
 
 		connection.end();
+	}
+
+	app.get('/produtos', listaProdutos);
+
+	app.get('/produtos/form', function(req, res) {
+		res.render('produtos/form')
+	});
+
+	app.post('/produtos', function(req, res) {
+		var connection = app.infra.connectionFactory();
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+		var produto = req.body;
+
+		produtosDAO.salva(produto, function(err, results) {
+			if (err) throw err;
+
+			res.redirect('/produtos');
+		});
 	});
 }
