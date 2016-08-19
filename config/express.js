6ed5a44ec61c5f0966b4999623dd1db7edd71823
Leonardo Cadastro <Leonardo Cadastro>
2016-08-19 00:32:52 -0300

    adicionado middleware para validacao de erros

--- config/express.js
+++ config/express.js
@@ -18,5 +18,16 @@ module.exports = function() {
 		.then('infra')
 		.into(app);
 
+	app.use(function(req, res, next) {
+		res.status(404).render('erros/404');
+	});
+
+	app.use(function(error, req, res, next) {
+		if(process.env.NODE_ENV == 'production'){
+			res.status(500).render('erros/500');
+		}
+		next(error);
+	});
+
 	return app;
 }
